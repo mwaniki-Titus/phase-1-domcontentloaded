0c106c5927a772fa76a9e06c435c788259d19a49
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-06-22 12:29:36 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function (){
+    const myElement = document.getElementById('text');
+
+    myElement.textContent = 'This is really cool!';
+
+});
